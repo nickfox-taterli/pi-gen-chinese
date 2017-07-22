1b12caef67480c4a436f3a08181b13e1f9bc2556
Tater Li <admin@lijingquan.net>
2017-07-22 16:36:08 +0800

    固定boot分区为60M,防止Windows提示磁盘错误.

--- export-image/prerun.sh
+++ export-image/prerun.sh
@@ -8,10 +8,10 @@ rm -f ${IMG_FILE}
 rm -rf ${ROOTFS_DIR}
 mkdir -p ${ROOTFS_DIR}
 
-BOOT_SIZE=$(du --apparent-size -s ${EXPORT_ROOTFS_DIR}/boot --block-size=1 | cut -f 1)
+BOOT_SIZE=60
 TOTAL_SIZE=$(du --apparent-size -s ${EXPORT_ROOTFS_DIR} --exclude var/cache/apt/archives --block-size=1 | cut -f 1)
 
-IMG_SIZE=$((BOOT_SIZE + TOTAL_SIZE + (800 * 1024 * 1024)))
+IMG_SIZE=$(((BOOT_SIZE * 1024 * 1024) + TOTAL_SIZE + (800 * 1024 * 1024)))
 
 truncate -s ${IMG_SIZE} ${IMG_FILE}
 fdisk -H 255 -S 63 ${IMG_FILE} <<EOF
@@ -20,7 +20,7 @@ n
 
 
 8192
-+$((BOOT_SIZE * 2 /512))
++60M
 p
 t
 c
